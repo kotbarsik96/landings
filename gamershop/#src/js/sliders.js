adeb32da3185c7bcdb3b19099b840c91641db0d4
kotbarsik96 <kotbarsik96@gmail.com>
2022-09-05 12:03:35 +0400

    added lazy swiper load to gamershop

--- gamershop/#src/js/sliders.js
+++ gamershop/#src/js/sliders.js
@@ -1,19 +1,35 @@
-const heroSlider = new Swiper('.choose-hero', {
-    slidesPerView: 1,
-
-    pagination: {
-        el: '.choose-hero-pag',
-        bulletClass: 'choose-hero-pag__bullet',
-        bulletActiveClass: 'choose-hero-pag__bullet--active',
-        type: 'bullets'
-    },
-
-    breakpoints: {
-        370: {
-            slidesPerView: 2
-        },
-        450: {
-            slidesPerView: 3
-        }
-    }
+// отложенная загрузка слайдера
+const swiperLoad = new Promise(resolve => {
+    setTimeout(() => {
+        const script = document.createElement("script");
+        script.src = "js/swiper-bundle.min.js";
+        document.body.append(script);
+        resolve();
+    }, 100);
 });
+swiperLoad.then(initSliders);
+
+function initSliders() {
+    if (typeof Swiper === "undefined") return setTimeout(initSliders, 100);
+
+    // HERO-SLIDER
+    const heroSlider = new Swiper('.choose-hero', {
+        slidesPerView: 1,
+
+        pagination: {
+            el: '.choose-hero-pag',
+            bulletClass: 'choose-hero-pag__bullet',
+            bulletActiveClass: 'choose-hero-pag__bullet--active',
+            type: 'bullets'
+        },
+
+        breakpoints: {
+            370: {
+                slidesPerView: 2
+            },
+            450: {
+                slidesPerView: 3
+            }
+        }
+    });
+}
